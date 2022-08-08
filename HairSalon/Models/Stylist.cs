aa8a56ad369ddec23894a90e1c58a74a8a6ed915
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-08-08 01:06:53 -0700

    update code via specifications

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -1,10 +1,13 @@
+using System.Collections.Generic;
+
 namespace HairSalon.Models
 {
   public class Stylist
   {
+    this.Clients = new HashSet<Client>();
+  }
     public int StylistId { get; set; }
     public string Description { get; set; }
     public int ClientsId { get; set; }
     public virtual Clients Clients { get; set; }
   }
-}
